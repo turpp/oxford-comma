05fd1ad75a2a59c53f5606010e32818995c1ca15
turpp <white.tripp@icloud.com>
2020-10-02 12:13:51 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,33 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#    array_string=array.join(" ")
+#     if array_string.include? " "
+#         array.insert(-2,"and")
+#         array.join(" ")
+#     else
+#         return array_string
+#     end
+   
+   
+# end
 
-end
+
+def oxford_comma(array)
+    case array.count
+    when 0
+      ""
+    when 1
+      array.join
+    when 2
+      array.join(' and ')
+    else
+      array_copy = array
+      array_copy[-1] = "and #{array_copy[-1]}"
+      array_copy.join(', ')
+    end
+  end
+
+
+
+
+
+
